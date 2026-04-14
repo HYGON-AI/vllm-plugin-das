36d0b342604622ca4b8938e187828a535958aa6c
zhuwenwen <1536492181@qq.com>
2026-04-14 14:37:39 +0800

    [Patches] Update the format of attention patches

--- vllm_hcu/patches/vllm__model_executor__layers__attention__attention.patch.py
+++ vllm_hcu/patches/vllm__model_executor__layers__attention__attention.patch.py
@@ -5,18 +5,20 @@ Patch for vllm.model_executor.layers.attention.attention unified_kv_cache_update
 """
 
 PATCHES = [
-    (
-        "import vllm.envs as envs",
-        "import vllm.envs as envs\nimport vllm_hcu.platforms.envs as henvs",
-    ),
+(
+    "import vllm.envs as envs",
+    "import vllm.envs as envs\nimport vllm_hcu.platforms.envs as henvs",
+),
 
-    (
-        "device=kv_cache.device, dtype=kv_cache.dtype",
-        """device=(
-            key.device if henvs.VLLM_HCU_USE_FA_UNIFIED_ATTENTION else kv_cache.device
-        ),
-        dtype=(
-            key.dtype if henvs.VLLM_HCU_USE_FA_UNIFIED_ATTENTION else kv_cache.dtype
-        )""",
-    ),
+(
+"""
+    return torch.empty(0, device=kv_cache.device, dtype=kv_cache.dtype)
+""",
+"""
+    if henvs.VLLM_HCU_USE_FA_UNIFIED_ATTENTION:
+        return torch.empty(0, device=key.device, dtype=key.dtype)
+    else:
+        return torch.empty(0, device=kv_cache.device, dtype=kv_cache.dtype)
+""",
+),
 ]
--- vllm_hcu/patches/vllm__model_executor__layers__attention__mla_attention.patch.py
+++ vllm_hcu/patches/vllm__model_executor__layers__attention__mla_attention.patch.py
@@ -6,24 +6,26 @@ vllm.model_executor.layers.attention.mla_attention process_weights_after_loading
 import re
 
 PATCHES = [
-    (
-        "import vllm.envs as envs",
-        "import vllm.envs as envs\nimport vllm_hcu.platforms.envs as henvs",
-    ),
+(
+    "import vllm.envs as envs",
+    "import vllm.envs as envs\nimport vllm_hcu.platforms.envs as henvs",
+),
 
-    (
-        """        kv_b_proj_weight = get_and_maybe_dequant_weights(
+(
+"""
+        kv_b_proj_weight = get_and_maybe_dequant_weights(
             self.kv_b_proj, out_dtype=act_dtype
         ).T
 """,
-        """        if henvs.VLLM_USE_NN:
+"""        
+        if henvs.VLLM_USE_NN:
             kv_b_proj_weight = get_and_maybe_dequant_weights(
                 self.kv_b_proj, out_dtype=act_dtype
-        )
+            )
         else:
             kv_b_proj_weight = get_and_maybe_dequant_weights(
-            self.kv_b_proj, out_dtype=act_dtype
-        ).T
+                self.kv_b_proj, out_dtype=act_dtype
+            ).T
 """,
     ),
 ]
