2a16372590134c415ef89eae58196f07c61aacfd
zhuwenwen <1536492181@qq.com>
2026-04-20 21:00:02 +0800

    [Envs] Remove VLLM_HCU_USE_DEFAULT_GEMM

--- vllm_hcu/patches/vllm__model_executor__layers__utils.patch.py
+++ vllm_hcu/patches/vllm__model_executor__layers__utils.patch.py
@@ -7,24 +7,11 @@ vllm.model_executor.layers.utils dispatch_unquantized_gemm
 PATCHES = [
 (
 """
-import torch
-""",
-"""
-import torch
-import vllm_hcu.platforms.envs as henvs
-""",
-),
-
-(
-"""
         return rocm_unquantized_gemm
 """,
 """
-        if henvs.VLLM_HCU_USE_DEFAULT_GEMM:
-            return default_unquantized_gemm
-        else:
-            return rocm_unquantized_gemm
-            
+        # return rocm_unquantized_gemm
+        return default_unquantized_gemm
 """,
 ),
 ]
