11ec9f95089140ada5762dc2fa2533c6f4837cb9
zhuwenwen <1536492181@qq.com>
2026-05-25 15:03:32 +0800

    [Patches] Update vllm__config__model.patch.py

--- vllm_hcu/patches/vllm__config__model.patch.py
+++ vllm_hcu/patches/vllm__config__model.patch.py
@@ -4,30 +4,30 @@ Patch for vllm.config.model
 """
 
 PATCHES = [
-    (
-        '''                "cpu_awq",
-            ]''',
-        '''                "cpu_awq",
+(
+ '''
+                "cpu_awq",
+ ''',
+ '''
+                "cpu_awq",
                 "slimquant_marlin",
                 "slimquant_compressed_tensors_marlin",
                 "slimquant_w4a8",
-            ]'''
-    ),
+ ''',
+),
+
 (
- '''  
+ '''
                 quantization_override = method.override_quantization_method(
                     quant_cfg, self.quantization, hf_config=self.hf_config
                 )
  ''',
- '''  
-                if name in ["slimquant_marlin", "slimquant_compressed_tensors_marlin", "slimquant_w4a8"]:
-                    quantization_override = method.override_quantization_method(
-                        quant_cfg, self.quantization
-                    )
-                else:
-                    quantization_override = method.override_quantization_method(
-                        quant_cfg, self.quantization, hf_config=self.hf_config
-                    )
+ '''
+                skip_hf_config = name in {"slimquant_marlin", "slimquant_compressed_tensors_marlin", "slimquant_w4a8"}
+                quantization_override = method.override_quantization_method(
+                    quant_cfg, self.quantization,
+                    **({} if skip_hf_config else {"hf_config": self.hf_config})
+                )
  ''',
 ),
 ]
