8f130b5d09b42f83e59bcbdb5152222834ebac86
zhangzbb <zhangzbb@hygon.cn>
2026-06-15 13:28:32 +0800

    fix(deepseek-r1-distill-llama-70b-tokenizer): centralize hook

--- vllm_hcu/__init__.py
+++ vllm_hcu/__init__.py
@@ -1,14 +1,10 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from .patch_utils import import_hook, patch_module_class_function
-from .patches.patch_deepseek_r1_distill_llama_70b_tokenizer import (
-    patch_deepseek_r1_distill_llama_70b_tokenizer,
-)
 
 def hcu_platform_plugin():
     """Register the HCU platform."""
     import_hook()
-    patch_deepseek_r1_distill_llama_70b_tokenizer()
     return "vllm_hcu.platforms.hcu.HCUPlatform"
 
 def hcu_platform_register_model():
